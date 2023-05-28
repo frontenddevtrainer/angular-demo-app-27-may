c70d786250fd2d1c0c43679d750de2a993204279
admin <admin@example.com>
2023-05-28 17:33:44 +0100

    Added header and footer component with updates to product listing.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,54 +9,64 @@ export class AppComponent {
 
   latestProducts = [{
     "id": 1,
-    "name": "Latex Rubber Gloves Size 9",
-    "price": 69,
-    "description": "ornare consequat lectus in est risus auctor sed tristique in tempus sit"
+    "name": "Potatoes - Yukon Gold, 80 Ct",
+    "price": 20,
+    "description": "neque aenean auctor gravida sem praesent id massa id nisl venenatis lacinia aenean sit amet",
+    "thumbnail": "http://dummyimage.com/250x250.png/cc0000/ffffff"
   }, {
     "id": 2,
-    "name": "Onions - White",
-    "price": 69,
-    "description": "a suscipit nulla elit ac nulla sed vel enim sit amet nunc viverra dapibus nulla suscipit ligula in"
+    "name": "Squash - Pattypan, Yellow",
+    "price": 12,
+    "description": "vel nisl duis ac nibh fusce lacus purus aliquet at feugiat non pretium quis lectus suspendisse",
+    "thumbnail": "http://dummyimage.com/250x250.png/5fa2dd/ffffff"
   }, {
     "id": 3,
-    "name": "Onions - Dried, Chopped",
-    "price": 67,
-    "description": "tincidunt lacus at velit vivamus vel nulla eget eros elementum pellentesque quisque porta volutpat erat"
+    "name": "Chicken - Bones",
+    "price": 46,
+    "description": "nibh fusce lacus purus aliquet at feugiat non pretium quis lectus suspendisse potenti in eleifend quam a odio in hac",
+    "thumbnail": "http://dummyimage.com/250x250.png/dddddd/000000"
   }, {
     "id": 4,
-    "name": "Tart Shells - Barquettes, Savory",
-    "price": 33,
-    "description": "augue vel accumsan tellus nisi eu orci mauris lacinia sapien quis libero nullam sit amet turpis elementum ligula"
+    "name": "Butter Ripple - Phillips",
+    "price": 63,
+    "description": "nulla facilisi cras non velit nec nisi vulputate nonummy maecenas tincidunt lacus at velit vivamus vel nulla eget eros elementum",
+    "thumbnail": "http://dummyimage.com/250x250.png/ff4444/ffffff"
   }, {
     "id": 5,
-    "name": "Muffin Mix - Blueberry",
-    "price": 14,
-    "description": "aenean lectus pellentesque eget nunc donec quis orci eget orci vehicula condimentum"
+    "name": "Cocoa Powder - Dutched",
+    "price": 94,
+    "description": "sed tincidunt eu felis fusce posuere felis sed lacus morbi sem",
+    "thumbnail": "http://dummyimage.com/250x250.png/dddddd/000000"
   }, {
     "id": 6,
-    "name": "Lamb Rack Frenched Australian",
-    "price": 22,
-    "description": "ac diam cras pellentesque volutpat dui maecenas tristique est et tempus semper est quam pharetra magna ac consequat metus sapien"
+    "name": "Parsley - Fresh",
+    "price": 78,
+    "description": "nullam porttitor lacus at turpis donec posuere metus vitae ipsum aliquam non mauris morbi non lectus aliquam",
+    "thumbnail": "http://dummyimage.com/250x250.png/ff4444/ffffff"
   }, {
     "id": 7,
-    "name": "Oil - Avocado",
-    "price": 42,
-    "description": "semper rutrum nulla nunc purus phasellus in felis donec semper sapien a libero"
+    "name": "Butter Ripple - Phillips",
+    "price": 95,
+    "description": "congue risus semper porta volutpat quam pede lobortis ligula sit amet eleifend pede",
+    "thumbnail": "http://dummyimage.com/250x250.png/5fa2dd/ffffff"
   }, {
     "id": 8,
-    "name": "Roe - White Fish",
-    "price": 69,
-    "description": "vitae consectetuer eget rutrum at lorem integer tincidunt ante vel ipsum praesent blandit lacinia erat vestibulum"
+    "name": "Wine - Periguita Fonseca",
+    "price": 23,
+    "description": "sapien urna pretium nisl ut volutpat sapien arcu sed augue aliquam erat volutpat",
+    "thumbnail": "http://dummyimage.com/250x250.png/dddddd/000000"
   }, {
     "id": 9,
-    "name": "Chivas Regal - 12 Year Old",
-    "price": 88,
-    "description": "in est risus auctor sed tristique in tempus sit amet sem fusce consequat nulla nisl nunc nisl duis bibendum felis"
+    "name": "Chicken Breast Wing On",
+    "price": 58,
+    "description": "ut at dolor quis odio consequat varius integer ac leo pellentesque ultrices mattis odio donec vitae nisi nam",
+    "thumbnail": "http://dummyimage.com/250x250.png/dddddd/000000"
   }, {
     "id": 10,
-    "name": "Cheese - St. Andre",
-    "price": 13,
-    "description": "nulla tellus in sagittis dui vel nisl duis ac nibh fusce lacus"
+    "name": "Cookie - Dough Variety",
+    "price": 30,
+    "description": "augue aliquam erat volutpat in congue etiam justo etiam pretium iaculis justo in hac habitasse",
+    "thumbnail": "http://dummyimage.com/250x250.png/cc0000/ffffff"
   }]
 
   topProducts = [{
--- src/app/utils/utils.module.ts
+++ src/app/utils/utils.module.ts
@@ -1,18 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ProductsListingComponent } from './products-listing/products-listing.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
-    ProductsListingComponent
+    ProductsListingComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     CommonModule
   ],
   exports : [
-    ProductsListingComponent
+    ProductsListingComponent,
+    HeaderComponent,
+    FooterComponent
   ]
 })
 export class UtilsModule { }
