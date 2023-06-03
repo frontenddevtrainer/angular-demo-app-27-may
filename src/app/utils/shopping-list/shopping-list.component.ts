a3e7fc2b8962a69ff8fd8ca6ffd4bfbad496a998
admin <admin@example.com>
2023-06-03 18:32:42 +0100

    Example for lifecycle hooks.

--- src/app/utils/shopping-list/shopping-list.component.ts
+++ src/app/utils/shopping-list/shopping-list.component.ts
@@ -7,7 +7,16 @@ import {
   query,
   stagger,
 } from '@angular/animations';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
 import { Product } from 'src/app/interfaces/products.interface';
 
 @Component({
@@ -15,34 +24,35 @@ import { Product } from 'src/app/interfaces/products.interface';
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.scss'],
   animations: [
-    trigger(
-      'fontSizeAnimation',
-      [
-        state('small', style({ fontSize: '10px', color: 'red', opacity: 0.5 })),
-        state(
-          'medium',
-          style({ fontSize: '15px', color: 'green', opacity: 1 })
-        ),
-        state(
-          'large',
-          style({ fontSize: '20px', color: 'blue', opacity: 0.3 })
-        ),
-        transition('small <=> medium', animate('500ms ease-in')),
-        transition('medium <=> large', animate('1500ms ease-out')),
-        transition('small <=> large', animate('1000ms ease-in')),
-      ],
-    ),
+    trigger('fontSizeAnimation', [
+      state('small', style({ fontSize: '10px', color: 'red', opacity: 0.5 })),
+      state('medium', style({ fontSize: '15px', color: 'green', opacity: 1 })),
+      state('large', style({ fontSize: '20px', color: 'blue', opacity: 0.3 })),
+      transition('small <=> medium', animate('500ms ease-in')),
+      transition('medium <=> large', animate('1500ms ease-out')),
+      transition('small <=> large', animate('1000ms ease-in')),
+    ]),
     trigger('listItems', [
-      transition("* <=> *", [
-        query(":enter", [
-          style({ opacity: 0, transform: "translateX(-15px)" }),
-          stagger("500ms", animate("500ms", style({ opacity: 1, transform: "translateX(0px)" })))
-        ])
-      ])
-    ])
+      transition('* <=> *', [
+        query(
+          ':enter',
+          [
+            style({ opacity: 0, transform: 'translateX(-15px)' }),
+            stagger(
+              '500ms',
+              animate(
+                '500ms',
+                style({ opacity: 1, transform: 'translateX(0px)' })
+              )
+            ),
+          ],
+          { optional: true }
+        ),
+      ]),
+    ]),
   ],
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit, OnChanges, OnDestroy {
   currentFontAnimationState = 'medium';
 
   @Input('items') itemsInCart: Product[] = [];
@@ -55,5 +65,17 @@ export class ShoppingListComponent {
 
   changeCurrentFontSize(size: string) {
     this.currentFontAnimationState = size;
+  }
+
+  ngOnInit(): void {
+    console.log('ngOnInit');
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges');
+  }
+
+  ngOnDestroy(): void {
+    console.log('ngOnDestroy');
   }
 }
